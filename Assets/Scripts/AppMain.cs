0225a9442fe82efb3056209230372f7b67b65b70
Gibby <chenzhibin@stu.hit.edu.cn>
2024-01-27 18:03:09 +0800

    appmain update

--- Assets/Scripts/AppMain.cs
+++ Assets/Scripts/AppMain.cs
@@ -24,12 +24,18 @@ public class AppMain : MonoBehaviour
         UIManager.Instance.Init();
         AudiManager.Instance.Init();
         RoleManager.Instance.Init();
+        GameConfigManager.Instance.init();
 
         // NOTE：这里的名字要和prefab的名字一致
         // 应该改为每一个UI脚本都有一个名字，用于注册Dict
         // 文件使用addressable管理（注意理解异步加载）
         UIManager.Instance.ShowUI<LoginView>("LoginUI");
         AudiManager.Instance.PlayBGM("bgm1");
+
+        //Test
+        string name = GameConfigManager.Instance.GetCardById("10001")["name"];
+        UnityEngine.Debug.Log("test:" + name);
+        //print(name);
     }
 
     void Update()
