f6a22c788204edc3bbed670d5a59faa074114170
Gibby <chenzhibin@stu.hit.edu.cn>
2024-01-27 17:51:38 +0800

    GameConfig update。

--- Assets/Scripts/AppMain.cs
+++ Assets/Scripts/AppMain.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class AppMain : MonoBehaviour
@@ -12,10 +13,18 @@ public class AppMain : MonoBehaviour
 
     void Start()
     {
+        //Init config table
+        GameConfigManager.Instance.init();
+
         // NOTE：这里的名字要和prefab的名字一致
         // 应该改为每一个UI脚本都有一个名字，用于注册Dict
         // 文件使用addressable管理（注意理解异步加载）
         UIManager.Instance.ShowUI<LoginView>("LoginUI");
+
+        //Test
+        string name = GameConfigManager.Instance.GetCardById("10001")["name"];
+        UnityEngine.Debug.Log("test:"+name);
+        //print(name);
     }
 
     void Update()
